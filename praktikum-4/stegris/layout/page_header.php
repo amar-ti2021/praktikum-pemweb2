6059bd7bcf393047d325297190416c49c5ffbac3
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-03-22 11:54:20 +0700

    Modify few files
    Modify title and brand to STEGRIS

--- praktikum-4/stegris/layout/page_header.php
+++ praktikum-4/stegris/layout/page_header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title><?php echo $title ?></title>
+    <title>STEGRIS | <?php echo $title ?></title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
@@ -16,7 +16,7 @@
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="index.php">Start Bootstrap</a>
+        <a class="navbar-brand ps-3" href="index.php">STEGRIS</a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
         <!-- Navbar Search-->
