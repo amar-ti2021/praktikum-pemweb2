1cfda86529dfded0693165f23cb74d902d9e9e99
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-05-30 19:40:22 +0700

    Create form create view for dosen

---
+++ praktikum-9/application/views/dosen/create.php
@@ -0,0 +1,88 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Halaman Daftar Dosen</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Blank Page</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Tambah Dosen</h3>
+            </div>
+            <div class="card-body">
+                <?= form_open('dosen/save'); ?>
+                <div class="form-group row">
+                    <label for="nidn" class="col-4 col-form-label">NIDN</label>
+                    <div class="col-8">
+                        <input id="nidn" name="nidn" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="nama" class="col-4 col-form-label">Nama</label>
+                    <div class="col-8">
+                        <input id="nama" name="nama" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-4">Gender</label>
+                    <div class="col-8">
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="gender" id="gender_0" type="radio" class="custom-control-input" value="L">
+                            <label for="gender_0" class="custom-control-label">Laki-Laki</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="gender" id="gender_1" type="radio" class="custom-control-input" value="P">
+                            <label for="gender_1" class="custom-control-label">Perempuan</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="tmp_lahir" class="col-4 col-form-label">Tempat Lahir</label>
+                    <div class="col-8">
+                        <input id="tmp_lahir" name="tmp_lahir" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="tgl_lahir" class="col-4 col-form-label">Tanggal Lahir</label>
+                    <div class="col-8">
+                        <input id="tgl_lahir" name="tgl_lahir" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="pendidikan" class="col-4 col-form-label">Pendidikan</label>
+                    <div class="col-8">
+                        <input id="pendidikan" name="pendidikan" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="offset-4 col-8">
+                        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+                <?= form_close(); ?>
+            </div>
+            <!-- /.card-body -->
+
+        </div>
+        <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
