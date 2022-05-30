bb355c49a053275a2429184567514ffcf74aaf4b
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-05-30 19:41:23 +0700

    Add AdminLTE card

--- praktikum-9/application/views/dosen/index.php
+++ praktikum-9/application/views/dosen/index.php
@@ -1,33 +1,66 @@
-<div class="col-md-12">
-    <h3>
-        Daftar Dosen
-    </h3>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>NIDN</th>
-                <th>Nama</th>
-                <th>Gender</th>
-                <th>Pendidikan</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $nomor = 1;
-            foreach ($list_dsn as $dsn) {
-            ?>
-                <tr>
-                    <td><?= $nomor ?></td>
-                    <td><?= $dsn->nidn ?></td>
-                    <td><?= $dsn->nama ?></td>
-                    <td><?= $dsn->gender ?></td>
-                    <td><?= $dsn->pendidikan ?></td>
-                </tr>
-            <?php
-                $nomor++;
-            }
-            ?>
-        </tbody>
-    </table>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Halaman Daftar Dosen</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Blank Page</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Daftar Dosen</h3>
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>NIDN</th>
+                            <th>Nama</th>
+                            <th>Gender</th>
+                            <th>Pendidikan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $nomor = 1;
+                        foreach ($list_dsn as $dsn) {
+                        ?>
+                            <tr>
+                                <td><?= $nomor ?></td>
+                                <td><?= $dsn->nidn ?></td>
+                                <td><?= $dsn->nama ?></td>
+                                <td><?= $dsn->gender ?></td>
+                                <td><?= $dsn->pendidikan ?></td>
+                            </tr>
+                        <?php
+                            $nomor++;
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
+
+        </div>
+        <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
 </div>
+<!-- /.content-wrapper -->
