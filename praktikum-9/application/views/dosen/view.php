622f983b65332093b99955332addc0cdb4b6a7da
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-05-30 19:52:09 +0700

    Create 'view' view for dosen

---
+++ praktikum-9/application/views/dosen/view.php
@@ -0,0 +1,62 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Halaman Daftar Dosen</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Blank Page</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Daftar Dosen</h3>
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>NIDN</th>
+                            <th>Nama</th>
+                            <th>Gender</th>
+                            <th>Tempat Lahir</th>
+                            <th>Tanggal Lahir</th>
+                            <th>Pendidikan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td><?= $dsn->nidn ?></td>
+                            <td><?= $dsn->nama ?></td>
+                            <td><?= $dsn->gender ?></td>
+                            <td><?= $dsn->tmp_lahir ?></td>
+                            <td><?= $dsn->tgl_lahir ?></td>
+                            <td><?= $dsn->pendidikan ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
+
+        </div>
+        <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
